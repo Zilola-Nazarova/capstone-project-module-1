45daf85dbd8b838e6b5dc661fdd44ab6baf64398
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-27 23:26:47 +0500

    Create coaches.js file and place there an array of objects based on the information of the Capstone_project_content.docx

---
+++ coaches.js
@@ -0,0 +1,38 @@
+const coachesData = [
+  {
+    name: 'Vadim Makhmudov',
+    position: 'One of the leading members of the Union of Architects of Uzbekistan recognized for his lifetime achievement in architecture',
+    responsibilities: 'You will explore together at least five design studios in Tashkent. There you will be able to work on real-world projects with architects and gain insight into the lives and work ethics of architects.',
+    photo: './images/speaker-vadim.jpg',
+  },
+  {
+    name: 'Alexandra Lee',
+    position: 'Art Specialist at Yeoju Technical Institute in Tashkent',
+    responsibilities: 'Practice a variety of drawing and painting techniques with Alexandra. At her workshops, both beginners and advanced artists work with different types of coloured pencils, papers, and other related materials.',
+    photo: './images/speaker-alexandra.jpg',
+  },
+  {
+    name: 'Shukur Djuraev',
+    position: 'Professor, Doctor of Architecture in History and Theory of Architecture, and Urban Planning',
+    responsibilities: 'Known for his remarkable talent for explaining extremely complicated ideas in simple terms, Shukur will be your principal instructor. Teaching methods include lectures, tutorials, practical demonstrations, fieldwork and e-learning.',
+    photo: './images/speaker-shukur.jpg',
+  },
+  {
+    name: 'Andrea Schmidt',
+    position: 'Docent in architecture and associate professor in theory and history of architecture at TIACE',
+    responsibilities: 'Understand architecture as both cultural expression and technical achievement. Vivid analyses of exemplary buildings and hands-on exercises in drawing and modelling will bring you closer to the work of architects.',
+    photo: './images/speaker-andrea.jpg',
+  },
+  {
+    name: 'Yulia Nazarova',
+    position: 'Certified coach and mentor with 8 years of experience in HR',
+    responsibilities: 'You will replace your ingrained unhealthy habits with new ones that will benefit your study, work and life. Yuliya will help you to cultivate a “growth mindset” and foster confidence in your abilities and responsibility for your actions.',
+    photo: './images/speaker-yulia.jpg',
+  },
+  {
+    name: 'Christina Tursunova',
+    position: 'Urban Planning Department Head, TIACE',
+    responsibilities: 'Christina will introduce you to the career development process and different professional planning tools. You will create a career plan that will help you move smoothly from high school to further education or employment.',
+    photo: './images/speaker-christina.jpg',
+  },
+]
