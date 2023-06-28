ee7be583219cb6dc9d8032311a920f7f08cfee7b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-28 15:40:55 +0500

    Adjust footer.js function, fix bugs in .hidden classes

--- footer.js
+++ footer.js
@@ -2,9 +2,13 @@ const mediaQuery = window.matchMedia('(min-width: 768px)');
 
 function screenTest(e) {
   if (e.matches) {
-    document.querySelector('.privacy-policy').classList.toggle('hidden');
-    document.querySelector('.terms-cookies').classList.toggle('hidden');
+    document.querySelector('.privacy-policy').classList.remove('hidden');
+    document.querySelector('.terms-cookies').classList.remove('hidden');
+  } else {
+    document.querySelector('.privacy-policy').classList.add('hidden');
+    document.querySelector('.terms-cookies').classList.add('hidden');
   }
 }
 
+screenTest(mediaQuery);
 mediaQuery.addListener(screenTest);
